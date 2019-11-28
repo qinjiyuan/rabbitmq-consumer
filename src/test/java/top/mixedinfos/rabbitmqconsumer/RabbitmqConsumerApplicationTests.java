933e13e05ed5037a32decb274e4273d62e02565b
qinjiyuan <jiyuan.qin@foxmail.com>
2019-11-28 09:08:45 +0800

    Initial commit

---
+++ src/test/java/top/mixedinfos/rabbitmqconsumer/RabbitmqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mixedinfos.rabbitmqconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
